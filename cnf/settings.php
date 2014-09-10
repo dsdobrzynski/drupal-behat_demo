45525ba813eda1551a0c733232dcb703a4b1c85c
Will Milton <wa.milton@gmail.com>
2014-09-10 13:50:04 -0500

    adding settings.php for connecting to mysql in vagrant box

---
+++ cnf/settings.php
@@ -0,0 +1,11 @@
+<?php
+
+require __DIR__ . '/vendor/autoload.php';
+
+$databases['default']['default'] = array(
+  'username' => 'default',
+  'password' => 'default',
+  'database' => 'default',
+  'host' => 'localhost',
+  'driver' => 'mysql'
+);
