b8a84fd420434ea5fc8ec62958d49b19ec18d641
bryrock <bryrock@346823.no-reply.drupal.org>
2014-09-12 09:54:47 -0500

    Tidy up the build script.

--- build/drush-build.sh
+++ build/drush-build.sh
@@ -20,12 +20,6 @@ env_path="$top_path/.env"
 drush_flags="-r $top_path/www"
 drush="drush $drush_flags"
 
-#Sanity Checks
-echo Build path is $build_path
-echo Top path is $top_path
-echo Environment config path is $env_path
-echo $top_path/www/sites/default/files
-
 # Load environment config 
 if [ -e "$top_path/.env" ]
 then
@@ -37,23 +31,12 @@ if [ -n "$install" ] && [ "$install" == "install" ]
 then
   echo "Install option is $install"
   echo "Building with install option"
-  source "$env_path"
   echo "Seed is $DROPSHIP_SEEDS"
-  echo "Preprocess CSS is $PREPROCESS_CSS"
-  echo "Preprocess JS is $PREPROCESS_JS"
   source "$install_path/install.sh"
 else
-  echo no install in progress \(aren\'t you glad?\)
+  echo no install in progress
 fi
 
 $drush kw-manifests
 echo "Clearing caches.";
-$drush cc all -y
-echo "Reverting all features.";
-$drush fra -y
-echo "Running any updates.";
-$drush updb -y
-#echo "Setting the theme default.";
-#$drush scr $build_path/scripts/default_set_theme.php
-echo "Clearing caches one last time.";
 $drush cc all -y
